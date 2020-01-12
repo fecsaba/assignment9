26861f32f60269d22dab156bd0868583816106b3
fecsaba <nexusegyesulet@gmail.com>
2020-01-13 00:48:02 +0100

    It works

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,92 @@
+<!-- Exercise 1 -->
+<!-- Create a Signup Form where you retrieve the following Information -->
+<!-- Full Name (First Name + Last Name) -->
+<!-- Mail -->
+<!-- Password -->
+<!-- Store Data? Yes/No -->
+
+<!-- Exercise 2 -->
+<!-- Only display the Form if it has NOT been submitted -->
+<!-- Display the Data Summary ONCE the Form HAS been submitted -->
+
+<!-- Exercise 3 -->
+<!-- Edit the Example from above and create a custom "Full Name" Control -->
+<!-- which still holds the First Name and Last Name Input Field -->
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <b-container>
+    <b-row align-h="center" v-if="!submitted">
+      <b-col cols="10">
+        <b-form>
+          <app-full-name v-model="person.fullName"></app-full-name>
+          <b-form-group label="Enter your email address" label-for="email_addr">
+            <b-form-input id="email_addr" v-model="person.email" trim />
+          </b-form-group>
+          <b-form-group label="Enter your Password" label-for="email_addr">
+            <b-form-input
+              id="password"
+              type="password"
+              v-model="person.password"
+              trim
+            />
+          </b-form-group>
+          <div class="text-center">
+            <b-form-group label="Store data?">
+              <b-form-radio-group
+                v-model="selected"
+                :options="options"
+                name="radio-inline"
+              >
+              </b-form-radio-group>
+            </b-form-group>
+            <div class="mt-3">
+              Selected: <strong>{{ selected }}</strong>
+            </div>
+
+            <b-button class="mt-5" variant="primary mt5"
+              @click="submitted = true">Submit</b-button>
+          </div>
+          <hr />
+        </b-form>
+      </b-col>
+    </b-row>
+    <b-row align-h="center" v-if="submitted">
+      <b-col cols="8">
+        <b-card title="Your data" tag="article" class="mt-5">
+          <b-card-text>Full name: {{ person.fullName }} </b-card-text>
+          <b-card-text>Email: {{ person.email }} </b-card-text>
+          <b-card-text>Password: {{ person.password }} </b-card-text>
+          <b-card-text>Store in Database?: {{ selected }} </b-card-text>
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import FullName from "@/components/FullName";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    appFullName: FullName
+  },
+  data: function() {
+    return {
+      person: {
+        fullName: "Who AmI",
+        email: "",
+        password: ""
+      },
+
+      selected: "No",
+      options: [
+        { text: "Yes", value: "Yes" },
+        { text: "No", value: "No" }
+      ],
+      submitted: false
+    };
   }
 };
 </script>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style />
---
+++ src/components/FullName.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <b-form-group label="Enter your first name" label-for="first_name">
+      <b-form-input
+        id="first_name"
+        :value="firstName"
+        @input="nameChanged(true, $event)"
+      />
+    </b-form-group>
+    <b-form-group label="Enter your last name" label-for="last_name">
+      <b-form-input
+        id="last_name"
+        :value="lastName"
+        @input="nameChanged(false, $event)"
+      />
+    </b-form-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FullName",
+  props: ["value"],
+  computed: {
+    firstName() {
+      return this.value.split(" ")[0];
+    },
+    lastName() {
+      return this.value.split(" ")[1];
+    }
+  },
+  methods: {
+    nameChanged(isFirst, event) {
+      console.log("event: " + event);
+      let name = "";
+      if (isFirst) {
+        name = event + " " + this.lastName;
+      } else {
+        name = this.firstName + " " + event;
+      }
+      this.value = name;
+
+      this.$emit("input", this.value);
+    }
+  }
+};
+</script>
+
+<style scoped />
